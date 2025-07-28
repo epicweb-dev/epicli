9425ec956e5d0ef0e2184b82eb6c5c78aeb1dfdb
Kent C. Dodds <me+github@kentcdodds.com>
2025-07-28 11:18:28 -0600

    fix(new): set version of create-remix
    Updated create-remix version to 2.16.8 in newProject function.
    
    Closes https://github.com/epicweb-dev/epic-stack/issues/1026

--- src/commands/new.ts
+++ src/commands/new.ts
@@ -3,7 +3,7 @@ import chalk from 'chalk'
 
 export async function newProject({ projectName }: { projectName?: string }) {
 	try {
-		const command = `npx --yes create-remix@latest --install --no-git-init --init-script --template epicweb-dev/epic-stack ${projectName ?? ''}`
+		const command = `npx --yes create-remix@2.16.8 --install --no-git-init --init-script --template epicweb-dev/epic-stack ${projectName ?? ''}`
 		execSync(command, { stdio: 'inherit' })
 	} catch (error) {
 		console.error(chalk.red('\n❌ Error:'), error)
